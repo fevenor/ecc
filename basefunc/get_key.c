40806a75a438af16c921608e374b2963af19ff6c
fevenor <fevenor@outlook.com>
2016-05-15 17:39:41 +0800

    修复错误

--- basefunc/get_key.c
+++ basefunc/get_key.c
@@ -10,29 +10,29 @@ key* get_key(enum curve_name ecname)
 	get_curve_parameters(ecname, c);
 	get_rand(c->length, key);
 	mpz_mod(key, key, c->n);
-	newkey = malloc(sizeof(key) + sizeof(char) *((c->length / 4 + 1) * 3 + 10));
+	newkey = malloc(sizeof(key) + sizeof(char) *((c->length / 8 + 1) * 3 + 10));
 	newkey->curve = malloc(sizeof(char) * 10);
-	newkey->private = malloc(sizeof(char)*(c->length / 4 + 1));
-	newkey->public_x = malloc(sizeof(char)*(c->length / 4 + 1));
-	newkey->public_y = malloc(sizeof(char)*(c->length / 4 + 1));
+	newkey->private = malloc(sizeof(char)*(c->length / 8 + 1));
+	newkey->public_x = malloc(sizeof(char)*(c->length / 8 + 1));
+	newkey->public_y = malloc(sizeof(char)*(c->length / 8 + 1));
 	scalar_mul_c(key, pub_p, ecname);
 	mpz_get_str(newkey->private, 16, key);
 	mpz_get_str(newkey->public_x, 16, pub_p->x);
 	mpz_get_str(newkey->public_y, 16, pub_p->y);
 	af_p_clears(pub_p);
-	if (ecname = 0)
+	if (ecname == 0)
 	{
 		newkey->curve = "secp160r2";
 	}
-	else if (ecname = 1)
+	else if (ecname == 1)
 	{
 		newkey->curve = "secp192r1";
 	}
-	else if (ecname = 2)
+	else if (ecname == 2)
 	{
 		newkey->curve = "secp224r1";
 	}
-	else if (ecname = 3)
+	else if (ecname == 3)
 	{
 		newkey->curve = "secp256r1";
 	}
